fbc7359f69e427acc0e92db439168e734723c710
Bruno Henrique D. Seco <bruuno14@msn.com>
2017-06-24 23:10:26 -0300

    ++

--- Calemas.Erp.Ui/src/views/midia/index.vue
+++ Calemas.Erp.Ui/src/views/midia/index.vue
@@ -144,32 +144,7 @@
             </div>
         </modal>
 
-        <modal title="Cadastro de Mídia" v-model="crud.create.modalIsOpen" effect="fade/zoom">
-            <div slot="modal-header" class="modal-header">
-                <h4 class="modal-title">Cadastro de Mídia</h4>
-            </div>
-            <form type="post" v-on:submit.prevent="crud.create.executeAction()">
-                <div class="row">
-                    <div class="form-group col-md-12">
-                        <label for="Nome">Nome</label>
-                        <input type="text" class="form-control" name="Nome" placeholder="Nome" v-model="crud.create.model.Nome">
-                    </div>
-                    <div class="form-group col-md-12">
-                        <label class="custom-control custom-checkbox">
-                            <input type="checkbox" name="Ativo" class="custom-control-input" v-model="crud.create.model.Ativo">
-                            <span class="custom-control-indicator"></span>
-                            <span class="custom-control-description"> Ativo?</span>
-                        </label>
-                    </div>
-                </div>
-            </form>
-            <div slot="modal-footer" class="modal-footer">
-                <button type="button" class="btn btn-default" @click="crud.create.modalIsOpen = false">Fechar</button>
-                <button type="button" class="btn btn-success" @click="crud.create.executeAction()">
-                    <i class="fa fa-check"></i> Salvar
-                </button>
-            </div>
-        </modal>
+        <create :crud="crud"/>
 
         <modal title="Exclusão de Mídia" v-model="crud.delete.modalIsOpen" effect="fade/zoom">
             <div slot="modal-header" class="modal-header">
@@ -205,10 +180,11 @@
     import pagination from 'vue-pagination-bootstrap'
     import { Api } from '../../common/api'
     import { Crud } from '../../common/crud'
+    import create from './create/create'
 
     export default {
         name: 'midia',
-        components: { modal, pagination },
+        components: { modal, pagination, create },
         data() {
             return {
                 crud: new Crud({ resource: "midia", vm: this }),
